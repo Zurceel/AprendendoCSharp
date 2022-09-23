776b96a39bbbc3e48f06d679981a801f85e65963
Zurceel <gabrielcruzsil@gmail.com>
2022-09-23 15:51:58 -0300

    Commitando o item

---
+++ 2-CriandoVariaveis/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Programa
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Aula 2 - Criando variáveis.");
+
+		int idade;
+
+		idade = 27;
+		Console.WriteLine(idade);
+
+		idade = 27 - 3;
+		Console.WriteLine(idade);
+
+		idade = 2 * 3;
+		Console.WriteLine(idade);
+
+		idade = (5 + 4) * 3;
+		Console.WriteLine(idade);
+
+		Console.WriteLine("Clique na telca enter para fechar ...");
+		Console.ReadLine();
+	}
+}
---
+++ 3 - CriandoVariaveisPontoFlutuante/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Programa
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Projeto da aula 3 - Variáveis Ponto Flutuante");
+
+		double  salario;
+
+		salario = 1.650;
+
+		double divisao;
+
+		divisao = 7.0 / salario;
+
+		Console.WriteLine(divisao);
+
+		Console.WriteLine(salario);
+
+		Console.WriteLine("Clique na telca enter para fechar ...");
+		Console.ReadLine();
+	}
+}
