68ed555cdefc88b5dc45d135d81907907f53d67c
Zurceel <gabrielcruzsil@gmail.com>
2022-09-23 17:20:25 -0300

    Conversão e outros tipos numéricos

---
+++ 4-ConversoesOutrosTipos/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Programa
+{
+	static void Main(string[] args)
+	{
+
+		double salario = 3000.61;
+		Console.WriteLine("Meu salário é " + salario);
+
+		int salarioInteiro = (int)salario;
+		Console.WriteLine("Meu salário inteiro é " + salarioInteiro);
+
+		long x = 3412012012910000000;
+		Console.WriteLine("Quanto eu queria ter na conta " + x);
+
+		float peso = 100.72f;
+		Console.WriteLine("Meu peso é " + peso);
+
+		Console.WriteLine("Pressione a tecla Enter para fechar ...");
+		Console.ReadLine();
+	}
+}
