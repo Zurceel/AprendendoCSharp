fe3f9a2583a8edce2acd77646ffdcb9f0126b6d9
Zurceel <gabrielcruzsil@gmail.com>
2022-09-29 18:09:08 -0300

    Aprendendo caracteres e textos.

---
+++ 5-CaracteresETextos/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Programa
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Executando o progeto 5 - Caracteres e textos");
+
+		char letra = 'a';
+		Console.WriteLine(letra);
+
+		letra = (char)65;
+        Console.WriteLine(letra);
+
+		letra = (char)(65 + 1);
+        Console.WriteLine(letra);
+
+        Console.WriteLine("Tecle enter para fechar");
+		Console.ReadLine();
+	}
+}
