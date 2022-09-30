e8ef4a0ccab00ad58d9011b9c94e42ff1a758d67
Zurceel <gabrielcruzsil@gmail.com>
2022-09-30 14:15:03 -0300

    Aprendendo a atribuir variaveis.

--- texte/Program.cs
+++ texte/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Programa
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Executando projeto 6 - Atribuições de variáveis");
+
+		int idade = 23;
+		int idadeGabriel = idade;
+
+		Console.WriteLine(idadeGabriel);
+
+		string parcela_1 = "10";
+		string parcela_2 = "20";
+
+		Console.WriteLine(parcela_1 + parcela_2);
+		
+		Console.WriteLine("Tecle enter para fechar ...");
+		Console.ReadLine();
+	}
+}
