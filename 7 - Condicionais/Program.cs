b6ca69e4e0df8e9c61ae38a2175dce5644befc95
Zurceel <gabrielcruzsil@gmail.com>
2022-09-30 14:38:56 -0300

    if/else

---
+++ 7 - Condicionais/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Programa
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Executando projeto 7 - Condicionais");
+
+		int idadeJoao = 16;
+		int quantidadePessoas = 0;
+
+		if (idadeJoao >= 18)
+		{
+			Console.WriteLine("Entrada liberada!");
+		}
+		else
+		{
+			if (quantidadePessoas >= 1)
+			{
+				Console.WriteLine("Ele está acompanhado. Entrada Liberada!");
+			}
+			else
+			{
+				Console.WriteLine("Entrada permitida apenas para maiores de 18 anos. Ou acompanhado por um maior");
+			}
+		}
+
+	}
+}
