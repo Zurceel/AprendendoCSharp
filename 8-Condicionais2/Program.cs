8052f52c6051471f840f8279bbb23cfe94cbdde3
Zurceel <gabrielcruzsil@gmail.com>
2022-09-30 18:32:03 -0300

    Fazendo o desafio da aula

---
+++ 8-Condicionais2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq.Expressions;
+
+class Programa
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Executando o projeto 8 - Condicionais 2");
+
+		int idadeJoao = 16;
+		int quantidadePessoas = 1;
+
+		if(idadeJoao >= 18 || quantidadePessoas > 0)
+		{
+			Console.WriteLine("Entrada liberada");
+		}
+		else
+		{
+			Console.WriteLine("Entrada permitida para maiores de 18 anos ou acompanhado por um responsável");
+		}
+	}
+}
---
+++ 9-Escopo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Programa
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Executando o projeto 9 - Escopo");
+
+		int idadeJoao = 16;
+		int quantidadePessoas = 0;
+
+		bool acompanhado = quantidadePessoas > 0;
+
+		string textoAdicional;
+
+		if (acompanhado == true)
+		{
+			textoAdicional = "João está acompanhando";
+		}
+		else
+		{
+			textoAdicional = "João não está acompanhando";
+		}
+
+		if (idadeJoao >= 18 || acompanhado)
+		{
+			Console.WriteLine(textoAdicional);
+			Console.WriteLine("Entrada liberada!");
+		}
+		else
+		{
+			Console.WriteLine(textoAdicional);
+			Console.WriteLine("Entrada permitida para maiores de 18 anos ou acompanhado por um responsável");
+		}
+	}
+}
