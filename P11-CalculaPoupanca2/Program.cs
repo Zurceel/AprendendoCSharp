bff1b9ab6cd3c564f05ad626c9914f4cc855d0d1
Zurceel <gabrielcruzsil@gmail.com>
2022-09-30 22:16:51 -0300

    Aprendendo laços de repetição, while/for"

---
+++ P11-CalculaPoupanca2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Programa
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Executando o projeto 11 - Calcula Poupança");
+
+		double investimento = 1000;
+		int mes = 1;
+
+		while (mes <= 12)
+		{
+			investimento = investimento + investimento * 0.005;
+			Console.WriteLine("No mes " + mes + " você tem " + investimento);
+
+			mes++;
+		}
+
+		Console.WriteLine("Clique na telca enter para fechar ...");
+		Console.ReadLine();
+	}
+}
