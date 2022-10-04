1797604ea76a874392baa0dd2ad4a2594a82b7e9
Zurceel <gabrielcruzsil@gmail.com>
2022-10-04 17:29:52 -0300

    Fazendo o desafio da aula

---
+++ DesafioFor/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Programa
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine("Desafio for");
+
+		for (int numero = 3; numero < 100; numero++)
+		{
+
+
+			if (numero % 3 == 0)
+			{
+				Console.Write(numero);
+			}
+
+		}
+		Console.WriteLine();
+
+		Console.WriteLine("Pressione a tecla Enter para fechar ...");
+		Console.ReadLine();
+	}
+}
